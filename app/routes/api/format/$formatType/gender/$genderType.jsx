9b243b1eb4a478ec25c7bc6a91d0fd30e6e7cbd5
Zack Hitchcock <zmhitchcock+git@gmail.com>
2022-03-26 21:34:33 -0700

    updated gender api route

--- app/routes/api/format/$formatType/gender/$genderType.jsx
+++ app/routes/api/format/$formatType/gender/$genderType.jsx
@@ -5,17 +5,24 @@ import axios from "axios";
 export const loader = async ({ params }) => {
   // handle "GET" request
   const res = await axios.get(
-    "https://randomuser.me/api/?results=5000&exc=email,login,registered,phone,cell,picture,id"
+    `https://randomuser.me/api/?format=${params.formatType}&results=50&exc=email,login,registered,phone,cell,picture,id`
   );
-  // console.log(res);
-  const data = res.data.results;
 
+  let data = null;
+  let genderInPopulation = null;
+  let genderData = null;
+  if (params.formatType === "json") {
+    data = res.data.results;
+    genderInPopulation = data.filter(
+      (user) => user.gender === params.genderType
+    );
+    genderData = genderInPopulation;
+    return json(genderData, { success: true }, 200);
+  } else if (params.formatType === "csv") {
+    data = res;
+    genderInPopulation = data;
+    genderData = genderInPopulation;
+    return new Response(genderData.data);
+  }
   console.log(params);
-
-  const genderInPopulation = data.filter(
-    (user) => user.gender === params.genderType
-  );
-  const genderData = genderInPopulation;
-
-  return json(genderData, { success: true }, 200);
 };
